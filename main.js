1f24cc814177dc3149ad0cfce7ff9539a0cbd24b
Yuval Saraf <unimonkiez@gmail.com>
2016-10-06 15:40:15 +0300

    Fix fridays

--- main.js
+++ main.js
@@ -1,15 +1,17 @@
 alert(
   [].slice.call(
     document.getElementById('mainIFrame').contentWindow.document.getElementsByClassName('cDM')
-  ).filter(function(span) {
-    return span.innerHTML !== '&nbsp;'
-  }).map(function(span) {
-    var html = span.innerHTML;
+  ).filter(function(td) {
+    return td.innerHTML !== '&nbsp;'
+  }).map(function(td) {
+    var html = td.innerHTML;
     if (html.match(/[0-9]:[0-9]{2}/)) {
       var split = html.split(':');
       var hours = Number(split[0]);
       var minutes = Number(split[1]);
-      return hours * 60 + minutes - (9 * 60);
+      var wholeTd = td.parentNode.parentNode.parentNode.parentNode;
+      var isFriday = Array.from(wholeTd.parentNode.childNodes).indexOf(wholeTd) === 7;
+      return (hours * 60) + minutes - (isFriday ? 0 : (9 * 60));
     } else {
       return false;
     }
